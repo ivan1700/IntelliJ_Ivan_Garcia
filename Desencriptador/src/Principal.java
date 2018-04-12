2dd82678cf5e2530e945163efb83e45beb5b1324
ivan1700 <iivan1700@gmail.com>
2018-04-12 19:41:25 +0200

    12/04/2018 Desecriptador (versión orientada a objetos) terminado.

--- Desencriptador/src/Principal.java
+++ Desencriptador/src/Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
-        Encriptado e=new Encriptado("(vI)(na) (raG)(aic) (eB)(nel)");
+        Encriptado e = new Encriptado("(vI)(na) (raG)(aic) (eB)(nel)");
         System.out.println(e.desencriptarFrase(e));
     }
 }
