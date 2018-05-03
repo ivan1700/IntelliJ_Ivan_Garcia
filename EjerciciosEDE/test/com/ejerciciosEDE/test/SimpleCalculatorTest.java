03d0193292cd8eef0497fe3690de041db4645fa5
ivan1700 <iivan1700@gmail.com>
2018-05-03 20:57:02 +0200

    03/05/18 EDE

--- EjerciciosEDE/test/com/ejerciciosEDE/test/SimpleCalculatorTest.java
+++ EjerciciosEDE/test/com/ejerciciosEDE/test/SimpleCalculatorTest.java
@@ -58,8 +58,15 @@ public class SimpleCalculatorTest {
 
     //Ejercicio 4
     @Test
-    public void isEvenNumber(){
+    public void isEvenNumber0(){
         int number=2;
+        boolean actual=this.calculator.isEvenNumber(number);
+        assertTrue("Es false debido a que el número es impar",actual);
+    }
+
+    @Test
+    public void isEvenNumber1(){
+        int number=23;
         boolean actual=this.calculator.isEvenNumber(number);
         assertTrue("Es false debido a que el número es impar",actual);
     }
