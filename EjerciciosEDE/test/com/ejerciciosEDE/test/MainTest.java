2b3ca3f42219f4a028d6476f14a2ad88739886d7
ivan1700 <iivan1700@gmail.com>
2018-05-10 20:58:19 +0200

    10/05/18 EDE

--- EjerciciosEDE/test/com/ejerciciosEDE/test/MainTest.java
+++ EjerciciosEDE/test/com/ejerciciosEDE/test/MainTest.java
@@ -2,13 +2,66 @@ package com.ejerciciosEDE.test;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class MainTest {
 
     @Test
-    public void test(){
-        Main.captureItemsBetweenDashes("hola-que-tal");
-      
+    public void MainTest0(){
+        String cadena= "";
+        List<String> esperado = Main.captureItemsBetweenDashes(cadena);
+        List<String> actual= new ArrayList<>();
+        assertEquals(esperado,actual);
+    }
+
+    @Test
+    public void MainTest1(){
+        String cadena= "-";
+        List<String> esperado = Main.captureItemsBetweenDashes(cadena);
+        List<String> actual= new ArrayList<>();
+        assertEquals(esperado,actual);
+    }
+    @Test
+    public void MainTest2(){
+        String cadena= "--";
+        List<String> esperado = Main.captureItemsBetweenDashes(cadena);
+        List<String> actual= new ArrayList<>();
+        assertEquals(esperado,actual);
+    }
+    @Test
+    public void MainTest3(){
+        String cadena= "-1-";
+        List<String> esperado = Main.captureItemsBetweenDashes(cadena);
+        List<String> actual= new ArrayList<>();
+        actual.add("1");
+        assertEquals(esperado,actual);
+    }
+    @Test
+    public void MainTest4(){
+        String cadena= "-1--";
+        List<String> esperado = Main.captureItemsBetweenDashes(cadena);
+        List<String> actual= new ArrayList<>();
+        actual.add("1");
+        assertEquals(esperado,actual);
+    }
+    @Test
+    public void MainTest5(){
+        String cadena= "1-aaa-3";
+        List<String> esperado = Main.captureItemsBetweenDashes(cadena);
+        List<String> actual= new ArrayList<>();
+        actual.add("aaa");
+        assertEquals(esperado,actual);
+    }
+    @Test
+    public void MainTest6(){
+        String cadena= "-1-aaa-3-";
+        List<String> esperado = Main.captureItemsBetweenDashes(cadena);
+        List<String> actual= new ArrayList<>();
+        actual.add("1");
+        actual.add("3");
+        assertEquals(esperado,actual);
     }
 }
