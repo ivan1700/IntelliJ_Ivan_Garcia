60bdc0c160599b6319531d9d900a94e5f4324879
ivan1700 <iivan1700@gmail.com>
2018-05-18 17:08:00 +0200

    18/05/18 Boletin Ficheros Al fin copon

--- BoletinFicheros/src/EntradasFicheroAleatorio.java
+++ BoletinFicheros/src/EntradasFicheroAleatorio.java
@@ -1,11 +1,9 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EntradasFicheroAleatorio implements EntradaArchivador {
-    private static int CONTADOR = 0;
+
     private static int STITULO = 30;
     private static int SCONTENIDO = 50;
     private static int SPUNTUACION = 10;
@@ -14,43 +12,95 @@ public class EntradasFicheroAleatorio implements EntradaArchivador {
 
     @Override
     public List<Entrada> leerDeArchivo(File f) {
-        return null;
+        List<Entrada>entradas =new ArrayList<>();
+        try(RandomAccessFile raf=new RandomAccessFile(f,"r");) {
+            int contador=0;
+            while (true){
+                raf.seek(contador * SALTO);
+                String titulo=raf.readUTF();
+                raf.seek((contador * SALTO) + STITULO);
+                String contenido=raf.readUTF();
+                raf.seek((contador * SALTO) + STITULO + SCONTENIDO);
+                int puntuacion=raf.readInt();
+                raf.seek((contador * SALTO) + STITULO + SCONTENIDO + SPUNTUACION);
+                String autor=raf.readUTF();
+                contador++;
+                Entrada e=new Entrada(titulo,contenido,puntuacion,autor);
+                entradas.add(e);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (EOFException e){
+            System.out.println("Fin del fichero");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return entradas;
     }
 
     @Override
     public List<Entrada> anyadirEntrada(File f, List<Entrada> entradas, Entrada entrada) {
 
         entradas.add(entrada);
-        for (Entrada e :
-                entradas) {
-            try (RandomAccessFile raf = new RandomAccessFile(f, "rw")) {
-                raf.seek(CONTADOR * SALTO);
-                raf.writeUTF(e.getTitulo());
-                raf.seek((CONTADOR * SALTO) + STITULO);
-                raf.writeUTF(e.getContenido());
-                raf.seek((CONTADOR * SALTO) + STITULO + SCONTENIDO);
-                raf.writeInt(e.getPuntuacion());
-                raf.seek((CONTADOR * SALTO) + STITULO + SCONTENIDO + SPUNTUACION);
-                raf.writeUTF(e.getAutor());
-                raf.seek((CONTADOR * SALTO) + STITULO + SCONTENIDO + SPUNTUACION + SAUTOR);
-                CONTADOR++;
-
-            } catch (FileNotFoundException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
+        try(RandomAccessFile raf = new RandomAccessFile(f,"rw")) {
+            int contador=0;
+            for(Entrada e : entradas){
+                raf.seek(contador*SALTO);
+                    raf.writeUTF(e.getTitulo());
+                raf.seek((contador*SALTO)+STITULO);
+                    raf.writeUTF(e.getContenido());
+                raf.seek((contador*SALTO)+STITULO+SCONTENIDO);
+                    raf.writeInt(e.getPuntuacion());
+                raf.seek((contador*SALTO)+STITULO+SCONTENIDO+SPUNTUACION);
+                    raf.writeUTF(e.getAutor());
+                contador++;
             }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return entradas;
     }
 
     @Override
     public List<Entrada> modificarEntrada(File f, List<Entrada> entradas, Entrada entrada) {
-        return null;
+        Entrada aBorrar = null;
+        for (Entrada e:
+             entradas) {
+            if(e.getTitulo().equals(entrada.getTitulo())){
+                aBorrar=e;
+            }
+        }
+        entradas.remove(aBorrar);
+        entradas.add(entrada);
+        try(RandomAccessFile raf = new RandomAccessFile(f,"rw")) {
+            int contador=0;
+            for(Entrada e : entradas){
+                raf.seek(contador*SALTO);
+                raf.writeUTF(e.getTitulo());
+                raf.seek((contador*SALTO)+STITULO);
+                raf.writeUTF(e.getContenido());
+                raf.seek((contador*SALTO)+STITULO+SCONTENIDO);
+                raf.writeInt(e.getPuntuacion());
+                raf.seek((contador*SALTO)+STITULO+SCONTENIDO+SPUNTUACION);
+                raf.writeUTF(e.getAutor());
+                contador++;
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (EOFException e){
+            System.out.println("Fin del fichero");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return entradas;
     }
 
     @Override
     public String tipoArchivo() {
-        return null;
+        return "Archivo aleatorio";
     }
 }
